8ebd2f2ed19acd229894c7607300d3831c13b1a2
Brian <bbg.visualjournalist@gmail.com>
2019-11-20 08:21:37 -0500

    Updated datasets. Renamed merged.js to main app.js

--- download-spreadsheet-data-app/app.js
+++ download-spreadsheet-data-app/app.js
@@ -1,35 +1,32 @@
+var _ = require('underscore');
 var fs = require('fs');
 var Tabletop = require('tabletop');
+var counter = 0;
+var dataIsPlural = [];
 
 
-
-// "sheets" refer to "sheets" in the google document
+// sheet names in the google document
 var sheets = ["Items", "Tags", "Countries", "Regions"];
+var dataIsPluralSpreadsheet = "1JiHp4bxS_KZY3Gwv4h2Ds65UrOO5sHgdg3xsxkENNys";
 
-var datasets = [
-	{"url": "1JiHp4bxS_KZY3Gwv4h2Ds65UrOO5sHgdg3xsxkENNys", "location": "../_data/"}
-	//{"name": "dataSources", "url": "1JiHp4bxS_KZY3Gwv4h2Ds65UrOO5sHgdg3xsxkENNys", "location": "../_data/"}
-	//{"name": "dataSources", "url": "1JiHp4bxS_KZY3Gwv4h2Ds65UrOO5sHgdg3xsxkENNys", "location": "../data/"},
-];
+var location = "../_data/";
 
 
 
 for (var k = sheets.length - 1; k >= 0; k-- ){
 	var sheet = sheets[k];
-	fetchData(sheet);
+	fetchDataIsPlural(sheet);
 }
 
+
+
 //Load data from google spreadsheet and write it to JSON files.
-function fetchData(sheetName){
+function fetchDataIsPlural(sheetName){
 	var currentSheet = sheetName;
-	var currentSheetFolder = sheetName + "/";
 
-	for (var i = datasets.length - 1; i >= 0; i--) {
-		loadSheetsAll(datasets[i].name, datasets[i].url, datasets[i].location);
-	}
+	loadSheetsAll(dataIsPluralSpreadsheet, location);
 
-
-	function loadSheetsAll(name, url, location){
+	function loadSheetsAll(url, location){
 
 		console.log('loading spreadsheet data: ' + currentSheet);
 
@@ -41,12 +38,9 @@ function fetchData(sheetName){
 		function loadSheet(data, tabletop) {
 			console.log("loading data from spreadsheet");
 
-			//loadedSectorData = data.Sheet1.elements;
-			//loadedSectorData = data[name].elements;
 			loadedSectorData = data[currentSheet].elements;
 
 			//Write updated data to .JSON files and update global variables.
-			var currentNumber = 0;
 			function writeJSON(){
 				console.log("trying to write: " + currentSheet);
 
@@ -59,15 +53,78 @@ function fetchData(sheetName){
 
 				fs.writeFileSync(filename, content);
 
+
+				if (currentSheet == "Items"){
+					dataIsPlural = loadedSectorData;
+				}
 			}
 			writeJSON();
-			console.log("Successfully exported .JSON files");
+			counter++;
+
+
+			console.log("Successfully exported .JSON files: " + counter);
+
+			if (counter == sheets.length ){
+				console.log("DONE importing 'Data is Plural'");
+				console.log("Load alternate datasets and merge them.");
+				fetchDataVOA();
+			}
+
 		};
 
 
 		var options = {
 			key: spreadsheet_url,
 			callback: loadSheet
+		};
+
+		Tabletop.init(options);
+
+	}
+
+}
+
+
+//Load data from google spreadsheet and write it to JSON files.
+function fetchDataVOA(){
+	loadSheetsVOA("1iGB4SXipfnw03tKBhtap8pflknY58dDe2MmCNzs0GZM", location);
+
+	function loadSheetsVOA(url, location){
+
+		console.log('\n\nloading VOA spreadsheet data: ');
+
+		var spreadsheet_url = url;
+
+		var myData;
+
+
+		function loadSheetVOA(data, tabletop) {
+			console.log("loading data from VOA spreadsheet");
+
+			loadedSectorData = data.Items.elements;
+
+			//Write updated data to .JSON files and update global variables.
+			function writeJSON(){
+				console.log("trying to write VOA spreadsheet: ");
+
+				var newJson = _.extend(dataIsPlural, loadedSectorData);
+				newJson = _.sortBy(newJson, 'edition');
+				// Save the data for baking into the HTML
+				//var filename = '../_data/' + sheetName + '.json';
+
+				var filename = location + 'Items.json';
+				const content = JSON.stringify(newJson);
+
+				fs.writeFileSync(filename, content);
+
+			}
+			writeJSON();
+		};
+
+
+		var options = {
+			key: spreadsheet_url,
+			callback: loadSheetVOA
 		};
 
 		Tabletop.init(options);
